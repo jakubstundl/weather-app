c5e0bf91567287ee5a852033820f0b016d4ef4cf
Jakub Stündl <jakub.stundl@gmail.com>
2023-03-28 12:03:23 +0200

    ip tracking for vercel1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
+  x0: string|string[];
   x1: string|string[];
   x2: string|string[];
   x3: string|string[];
@@ -13,6 +14,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const x0 = "Hello";
   const x1 = req.headers["x-forwarded-for"]||"no data";
   const x2 = req.headers["x-vercel-ip-country"]||"no data";
   const x3 = req.headers["x-vercel-ip-country-region"]||"no data";
@@ -20,5 +22,5 @@ export default function handler(
 
   
   
-  res.status(200).json({ x1,x2,x3,x4})
+  res.status(200).json({ x0,x1,x2,x3,x4})
 }
